2f6b0ce33af721c96b2a3a6aafecc7abcd48e49e
shradha mantry <itsmehak27@gmail.com>
2025-02-04 01:06:25 +0530

    logo-ticker and intro-section added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,24 +1,47 @@
 import Button from "@/components/button";
+import Image from "next/image";
 import { section } from "framer-motion/client";
-
+import designExample1Image from '@/assets/images/design-example-1.png';
+import designExample2Image from '@/assets/images/design-example-2.png';
+import Pointer from "@/components/Pointer";
 export default function Hero() {
     return (
-        <section className="py-24">
-            <div className="container">
+        <section className="py-24 overflow-x-clip">
+            <div className="container relative">
+                <div className="absolute -left-64 top-16 hidden lg:block">
+                    <Image 
+                    src={designExample1Image}
+                    alt="design example 1 image" 
+                    />
+                </div>
+                <div className="absolute -right-80 -top-16 hidden lg:block">
+                    <Image
+                    src={designExample2Image}
+                    alt="design example 2 image"></Image>
+                </div>
+                <div className="absolute top-96 left-48 hidden lg:block ">
+                    <Pointer name="Andrea" />
+                </div>
+                <div className="absolute right-56 hidden lg:block">
+                    <Pointer name="Bryan" color="red" />
+                </div>
+
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
                         ✨ $7.5M seed round raised
                     </div>
                 </div>
-                <h1 className="text-6xl font-medium text-center mt-6">
+                <h1 className="text-6xl md:text-7xl lg:text-8xl font-medium text-center mt-6">
                     Impactful design, created effortlessly
                 </h1>
-                <p className="text-center text-xl text-white/50 mt-8">
-                    Design tools shouldn&apos;t slow you down. Layers combine powerful features with an intuitive interface that keeps you in your creative flow.
-                </p> 
+                <div className="flex justify-center items-center">
+  <p className="text-center text-xl text-white/50 mt-8 max-w-2xl mx-auto">
+    Design tools shouldn&apos;t slow you down. Layers combine powerful features with an intuitive interface that keeps you in your creative flow.
+  </p>
+</div>
 
                 {/* Fixed Form */}
-                <form className="flex items-center border border-white/15 rounded-full p-2 mt-8 w-full max-w-md mx-auto">
+                <form className="flex items-center border border-white/15 rounded-full p-2 mt-8 w-full max-w-lg mx-auto ">
                     <input 
                         type="email" 
                         placeholder="Enter your email" 
