2ad03983fcbf6beca1830e9f64c29f7f933329b1
shradha mantry <itsmehak27@gmail.com>
2025-02-05 00:39:59 +0530

    fratures and integration-section added

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -1,3 +1,11 @@
+import { div, section, span } from "framer-motion/client";
+import FeatureCard from "@/components/FeatureCard";
+import avatar1 from '@/assets/images/avatar-ashwin-santiago.jpg'
+import avatar2 from '@/assets/images/avatar-lula-meyers.jpg'
+import avatar3 from '@/assets/images/avatar-florence-shaw.jpg'
+import Image from "next/image";
+import Tag from "@/components/Tag";
+import Avatar from "@/components/Avatar";
 const features = [
     "Asset Library",
     "Code Preview",
@@ -9,5 +17,77 @@ const features = [
 ];
 
 export default function Features() {
-    return <div>Features</div>;
+    return (
+        <section className="py-24">
+            <div className="container">
+                <div className="flex justify-center">
+                <Tag>Features</Tag>
+                </div>
+                <h2 className="text-6xl font-medium text-center mt-6 max-w-2xl mx-auto">Where power meets <span className="text-lime-400"> simplicity</span></h2>
+                <div className="mt-12 grid grid-cols-1 md:grid-cols-4 lg:grid-cols-3 gap-8">
+                    <FeatureCard  
+                    title="Real-time Colaboration"
+                    description="Work together seamlessly with conflict-free team editing." 
+                    className="md:col-span-2 lg:col-span-1" >
+                        <div className="aspect-video flex items-center justify-center">
+                            <Avatar className="z-40">
+                                <Image src={avatar1} alt="avatar1"
+                                className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-indigo-500 z-30">
+                                <Image src={avatar2} alt="avatar2" className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-amber-500 z-20">
+                                <Image src={avatar3} alt="avatar3" className="rounded-full" />
+                            </Avatar>
+                            <Avatar className="-ml-6 border-neutral-700">
+                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    {Array.from({length:3}).map((_,i)=>(
+                                        <span  key={i} className="size-1.5 rounded-full inline-flex bg-white"></span>
+
+                                    ))}
+                                </div>
+                            </Avatar>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard 
+                    title="Interactive Protoyping" 
+                    description="Engage your clients with prototypes that react to user actions." 
+                    className="md:col-span-2 lg:col-span-1"> 
+                        <div className="aspect-video flex items-center
+                        justify-center">
+                            <p className="text-4xl font-extrabold text-white/20 text-center"> We've achieved <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">incredible</span> growth this year</p>
+                        </div>
+                    </FeatureCard>
+                    <FeatureCard 
+                    title="Keyboard Quick Actions" 
+                    description="Powerful commands to help you create designs more quickly"
+                    className="md:col-span-2 md:col-start-2 lg:col-span-1 lg:col-start-auto"> 
+                    <div className="aspect-video flex items-center justify-center gap-4">
+                        <div className="size-14 inline-flex items-center justify-center bg-neutral-300 rounded-2xl text-xl text-neutral-950 w-28 font-medium ">
+                            shift
+                        </div>
+                        <div className="size-14 inline-flex items-center justify-center bg-neutral-300 rounded-2xl text-xl text-neutral-950 font-medium">
+                            alt
+                        </div>
+                        <div className="size-14 inline-flex items-center justify-center bg-neutral-300 rounded-2xl text-xl text-neutral-950 font-medium ">
+                            C
+                        </div>
+                    </div>
+
+                    </FeatureCard>
+                </div>
+                    <div className="mt-8 flex flex-wrap gap-3 justify-center">
+                        {features.map(feature =>(
+                            <div key={feature} className="bg-neutral-900 border border-white/10 inline-flex px-3 py-1.5 rounded-2xl gap-3 items-center md:px-5 md:py-2">
+                                <span className="bg-lime-400 text-neutral-950 size-5 rounded-full inline-flex items-center justify-center text-xl">&#10038;</span>
+                                <span className="font-medium md:text-lg">{feature}</span> 
+                            </div>
+
+                        ))}
+                    </div>
+                
+            </div>
+        </section>
+    );
 }
