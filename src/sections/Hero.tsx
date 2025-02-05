dba80eb39a0fa06cdb43027a8cfa0e6008ac5828
shradha mantry <itsmehak27@gmail.com>
2025-02-06 03:19:01 +0530

    hero-section animation added

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,30 +1,77 @@
-import Button from "@/components/button";
+"use client"
+import Button from "@/components/Button";
 import Image from "next/image";
 import { section } from "framer-motion/client";
 import designExample1Image from '@/assets/images/design-example-1.png';
 import designExample2Image from '@/assets/images/design-example-2.png';
 import Pointer from "@/components/Pointer";
+import {motion, useAnimate} from 'framer-motion'
+import { useEffect } from "react";
+import cursorYouImage from '@/assets/images/cursor-you.svg'
+import { url } from "inspector";
 export default function Hero() {
+    const [leftDesignScope, leftDesignAnimate]= useAnimate();
+    const [leftPointerScope, leftPointerAnimate]= useAnimate();
+    const [rightDesignScope,rightDesignAnimate]=useAnimate();
+    const [rightPointerScope, rightPointerAnimate]= useAnimate();
+    useEffect(() => {
+        leftDesignAnimate([
+            [leftDesignScope.current, {opacity:[1]},{duration:0.5}],
+            [leftDesignScope.current,{y:0,x:0},{duration:0.5}]
+        ]);
+        leftPointerAnimate([
+            [leftPointerScope.current,{opacity:1},{duration:0.5}],
+            [leftPointerScope.current,{y:0, x:-100},{duration:0.5,delay: 0.5 }],
+            [leftPointerScope.current,{x: 0 ,y: [0,16,0]},{duration: 0.5,ease: "easeInOut"},]
+        ]);
+        rightDesignAnimate([
+            [rightDesignScope.current, { opacity: 1 }, { duration: 0.5, delay: 1.5 }],
+            [rightDesignScope.current, { y: 0, x: 0 }, { duration: 0.5 }],
+        ]);
+        
+        rightPointerAnimate([
+            [rightPointerScope.current,{opacity:1},{duration:0.5,delay:1.5}],
+            [rightPointerScope.current,{y:0, x:175},{duration:0.5}],
+            [rightPointerScope.current,{x: 0 ,y: [0,20,0]},{duration: 0.5},]
+        ]);
+
+    }, []);
     return (
-        <section className="py-24 overflow-x-clip">
+        <section className="py-24 overflow-x-clip" style={{cursor:`url(${cursorYouImage.src}),auto`}}>
             <div className="container relative">
-                <div className="absolute -left-64 top-16 hidden lg:block">
+                <motion.div ref={leftDesignScope}
+                initial={{opacity:0 ,y:100,x:-100}} 
+                drag
+                className="absolute -left-64 top-16 hidden lg:block">
                     <Image 
                     src={designExample1Image}
                     alt="design example 1 image" 
+                    draggable="false"
                     />
-                </div>
-                <div className="absolute -right-80 -top-16 hidden lg:block">
+                </motion.div>
+               
+                <motion.div 
+                ref={rightDesignScope} 
+                initial={{opacity:0 ,y:100,x:100}}
+                drag
+                className="absolute -right-56 -top-16 hidden lg:block ">
                     <Image
                     src={designExample2Image}
+                    draggable="false"
                     alt="design example 2 image"></Image>
-                </div>
-                <div className="absolute top-96 left-48 hidden lg:block ">
+                </motion.div>
+
+             
+                <motion.div 
+                ref={leftPointerScope}
+                initial={{opacity:0 ,y:100,x:100}}
+                className="absolute top-96 left-48 hidden lg:block ">
                     <Pointer name="Andrea" />
-                </div>
-                <div className="absolute right-56 hidden lg:block">
+                </motion.div>
+                <motion.div ref={rightPointerScope}
+                initial={{opacity:0 ,y:100,x:275}} className="absolute right-56 hidden lg:block">
                     <Pointer name="Bryan" color="red" />
-                </div>
+                </motion.div>
 
                 <div className="flex justify-center">
                     <div className="inline-flex py-1 px-3 bg-gradient-to-r from-purple-400 to-pink-400 rounded-full text-neutral-950 font-semibold">
