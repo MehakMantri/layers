73bf244494a283ec0fc89a3b5089c5f8055f9885
shradha mantry <itsmehak27@gmail.com>
2025-02-11 01:07:43 +0530

    fixed-issues

--- src/sections/Introduction.tsx
+++ src/sections/Introduction.tsx
@@ -4,7 +4,7 @@ import Tag from "@/components/Tag";
 import { twMerge } from "tailwind-merge";
 import { useEffect, useRef, useState } from "react";
 
-const text = `You&apos;re racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
+const text = `You're racing to create exceptional work, but traditional design tools slow you down with unnecessary complexity and steep learning curves.`;
 const words = text.split(" ");
 
 export default function Introduction() {
